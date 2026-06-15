4d0eb99bffcb7df3efa44103f748e2f5f8436391
Gregory Munday <gregory.munday@gmail.com>
2026-06-15 23:49:02 +0100

    Fix markups

--- profsea/__init__.py
+++ profsea/__init__.py
@@ -9,7 +9,9 @@ logger = logging.getLogger("profsea")
 logger.setLevel(logging.INFO)
 
 # Use Rich!
-rich_handler = RichHandler(rich_tracebacks=True, show_time=True, show_path=False)
+rich_handler = RichHandler(
+    rich_tracebacks=True, show_time=True, show_path=False, markup=True
+)
 
 formatter = logging.Formatter("%(message)s")
 rich_handler.setFormatter(formatter)
